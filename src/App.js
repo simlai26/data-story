23430756f8506b8014af400fcdc3bd56a2a070c8
Ankit Muchhala <muchhalaankit@gmail.com>
2023-03-08 23:26:03 -0800

    add article

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <main>
+        <h1>
+        Does your immigration status affect your health in the United States?
+        </h1>
+        <p>- Samriddhi Simlai, University of Washington</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        Health insurance coverage is one of the most important predictors of preventive and therapeutic medical care. Several United States-based studies have found that uninsured individuals delay obtaining needed medical care, even putting off visits to a doctor when they are sick.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        <p>
+        Foreign-born individuals now make up almost 15% of the U.S. population. The 2021 U.S. Census shed light on how health insurance coverage for lawful immigrants is still very low. The main reason most immigrants came to America in 2022 was for employment opportunities. Other reasons were to escape a violent conflict, deal with environmental concerns, pursue educational opportunities, or reunite with family.
+        </p>
+
+        <p>
+        Non-citizens also are more likely than citizens to live in an urban area (96% vs. 86%). 
+        <strong>The visual below demonstrates how foreign-born individuals in metropolitan areas, including foreign-born citizens and non-citizens, are grossly more likely to be uninsured in the United States than native citizens.</strong>
+        </p>
+
+        <iframe className="juxtapose" width="100%" height="470px" src="https://cdn.knightlab.com/libs/juxtapose/latest/embed/index.html?uid=17d02326-be44-11ed-b5bd-6595d9b17862"></iframe>
+      
+        <p>
+        As a consequence of their higher uninsured rates, noncitizen immigrants face increased barriers to accessing health care and use less health care than citizens. Among nonelderly adults, noncitizens are significantly more likely than citizens to report not having a usual source of care (33% vs. 20%), not having had a doctor’s visit in the past 12 months (32% vs. 20%), and going without needed medical care in the past 12 months due to its cost (10% vs. 7%). 
+        </p>
+
+      <h3>
+      Why is this happening?
+      </h3>
+
+      <p>
+      According to research by the Kaiser Family Foundation, noncitizens are more likely to be uninsured than citizens because they have more limited access to private coverage due to working in jobs that are less likely to provide health benefits and they face eligibility restrictions for public coverage options, including Medicaid, the Children’s Health Insurance Program (CHIP), and Marketplace coverage. Additionally, those who are eligible for public coverage face a range of enrollment barriers including fear, confusion, and language and literacy challenges.
+      </p>
+
+      <p>
+      There has been some recent activity to expand access to health coverage for immigrants, but gaps clearly remain. Federal legislation has been proposed to expand eligibility for health coverage for immigrants, though it faces no clear path to passage in Congress. At the state level, a small but growing number of states have expanded fully state-funded coverage to certain low-income people regardless of immigration status. It will likely take time and sustained community-led efforts to rebuild trust and reduce fears surrounding the use of services among immigrant families. 
+      </p>
+      </main>
     </div>
   );
 }
